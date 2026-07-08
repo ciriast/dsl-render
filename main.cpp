783f6690750b6915e6f86c427e23e433e468737f
ciriast <ciriastorres@gmail.com>
2026-07-08 07:24:01 -0600

    Continue creating the window

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
+static SDL_Window *window = nullptr;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **app_state, int argc, char *argv[]) {
+    SDL_SetAppMetadata("Example Renderer clear", "1.0", "com.example.renderer-clear");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindow("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE)) {
+        SDL_Log("Couldn't create window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
